88ca3f48f963b540ea9226c384d51a6da1f6c74d
Leoanrdo Mosca <leonardo.mosca-1@unitn.it>
2024-10-29 18:37:02 +0100

    updated graphics, introduced searchbar, not working

--- GroupApp_frontend/src/app/topbar/topbar.component.ts
+++ GroupApp_frontend/src/app/topbar/topbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
