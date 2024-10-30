a3b77db459b5239d625d0d1bef7b4580663a6dac
Leoanrdo Mosca <leonardo.mosca-1@unitn.it>
2024-10-30 10:31:34 +0100

    first simple version of event card + some adjustments

--- GroupApp_frontend/src/app/app.component.ts
+++ GroupApp_frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from './topbar/topbar.component';
+import { EventcardComponent } from './eventcard/eventcard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopbarComponent],
+  imports: [RouterOutlet, TopbarComponent, EventcardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ GroupApp_frontend/src/app/eventcard/eventcard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventcard',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventcard.component.html',
+  styleUrl: './eventcard.component.css'
+})
+export class EventcardComponent {
+
+}
